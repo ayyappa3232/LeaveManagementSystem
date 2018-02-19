f690a4aa446ca91adf984faf61f7fd48d4605fd4
Ayyappa Penneti <ayyappakumar.penneti@gmail.com>
2018-02-19 18:09:48 +0530

    Created Registration Form

---
+++ LeaveManagementSystem/Controllers/AccountsController.cs
@@ -0,0 +1,53 @@
+﻿using LeaveManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LeaveManagementSystem.Controllers
+{
+    public class AccountsController : Controller
+    {
+        // GET: Accounts
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration(Registrationtbl reg)
+        {
+            string fileName = Path.GetFileNameWithoutExtension(reg.ImageFile.FileName);
+            string extension = Path.GetExtension(reg.ImageFile.FileName);
+            fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+            reg.ProfilePic = "~/Content/ProfileImages/" + fileName;
+            fileName = Path.Combine(Server.MapPath("~/Content/ProfileImages/"), fileName);
+            reg.ImageFile.SaveAs(fileName);
+
+            using (LeaveManagementSystemdatabase db = new LeaveManagementSystemdatabase())
+            {
+                db.Registrationtbls.Add(reg);
+                db.SaveChanges();
+                ModelState.Clear();
+                reg = null;
+                ViewBag.Message = "Successfully Registered";
+            }
+                return View(reg);
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Registrationtbl log)
+        {
+            return View();
+        }
+    }
+}
